0d808a4c099590d193bd50f5b1c09bd95bf3daf0
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-27 12:58:03 -0600

    Add DarkskyService with spec

---
+++ app/services/darksky_service.rb
@@ -0,0 +1,16 @@
+class DarkskyService
+  def retrieve_forecast(latlong)
+    parse_response("#{latlong}")
+  end
+
+  private
+
+  def parse_response(url, params = {})
+    response = connection.get(url, params)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def connection
+    Faraday.new("https://api.darksky.net/forecast/#{ENV['DARKSKY_API_KEY']}")
+  end
+end
