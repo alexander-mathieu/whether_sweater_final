0f29153471332d0f9dbe124ff2d68e340eaae31e
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-27 17:41:41 -0600

    Add cache to ForecastsController

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
-    facade = ForecastShowFacade.new(params[:location])
-    render json: ForecastSerializer.serialize(facade.location_forecast)
+    Rails.cache.fetch("#{params[:location]}-forecast", expires_in: 15.minutes) do
+      facade = ForecastShowFacade.new(params[:location])
+      render json: ForecastSerializer.serialize(facade.location_forecast)
+    end
   end
 end
