460ffee05265e436866c48524533a16c41fd3e0e
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-27 17:51:20 -0600

    Add location_forecast to ForecastShowFacade

--- app/facades/forecast_show_facade.rb
+++ app/facades/forecast_show_facade.rb
@@ -3,26 +3,30 @@ class ForecastShowFacade
     @location_params = location_params
   end
 
-  def forecast
-    darksky_service.retrieve_forecast(formatted_latlong)
+  def location_forecast
+    LocationForecast.new(forecast)
   end
 
   private
   attr_reader :location_params
+
+  def forecast
+    darksky_service.retrieve_forecast(formatted_latlong)
+  end
+
+  def darksky_service
+    DarkskyService.new
+  end
 
   def formatted_latlong
     "#{latlong[:lat]}," + "#{latlong[:lng]}"
   end
 
   def latlong
-    @latlong ||= google_maps_service.retrieve_latlong(location_params)
+    google_maps_service.retrieve_latlong(location_params)
   end
 
   def google_maps_service
-    @google_maps_service ||= GoogleMapsService.new
-  end
-
-  def darksky_service
-    @darksky_service ||= DarkskyService.new
+    GoogleMapsService.new
   end
 end
