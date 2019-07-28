abf295e94705aac65e3ef61c13fa5f8934b70337
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-28 15:50:30 -0600

    Add UsersController with create action and route

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: { api_key: user.api_key }, status: 201
+    else
+      render json: { error: 'Failed to create user' }, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
