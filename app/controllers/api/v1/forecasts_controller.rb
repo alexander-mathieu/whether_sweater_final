f62d1c53be7fb20b798eec367389ca2f1cd95067
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-27 18:15:31 -0600

    Remove cache from ForecastsController show action

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
-    Rails.cache.fetch("#{params[:location]}-forecast", expires_in: 15.minutes) do
-      facade = ForecastShowFacade.new(params[:location])
-      render json: ForecastSerializer.serialize(facade.location_forecast)
-    end
+    facade = ForecastShowFacade.new(params[:location])
+    render json: ForecastSerializer.serialize(facade.location_forecast)
   end
 end
