6ffa1128e3fa94937e887b26947ad8e4353baafc
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-28 16:40:22 -0600

    Add SessionsController with create action

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: user_params[:email])
+    
+    if user && user.authenticate(params[:password])
+      render json: { api_key: user.api_key }, status: 200
+    else
+      render json: { error: 'Failed to authenticate user' }, status: 401
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
+  end
+end
