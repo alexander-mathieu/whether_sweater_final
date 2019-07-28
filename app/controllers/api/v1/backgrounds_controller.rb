7ef162fb3cacad054afe22a1bb25bb914dedddc4
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-28 13:08:19 -0600

    Refactor BackgroundsController show action

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show
     facade = ForecastFacade.new(params[:location])
-    render json: BackgroundSerializer.new(facade.background_images)
+    render json: BackgroundSerializer.serialize(facade.background_images)
   end
 end
