7c70f0e26e5ef1c6399adc0d9314fb0f6ca7e554
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-28 15:55:17 -0600

    Resolve Hound complaints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  validates :email, presence: :true, uniqueness: :true
+  validates :email, presence: true, uniqueness: true
 
   has_secure_password
 
