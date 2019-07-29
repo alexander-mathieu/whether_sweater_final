066ca9ba849efb7ee36143b18f675b32aa0d8e30
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-28 20:16:05 -0600

    Update ForecastsController to reflect new ForecastFacade functionality

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
-    facade = ForecastFacade.new(params[:location])
+    facade = ForecastFacade.new(location_params)
     render json: ForecastSerializer.serialize(facade.location_forecast)
+  end
+
+  private
+
+  def location_params
+    params.permit(:location)
   end
 end
