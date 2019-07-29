f4b8af4baf21e01ce974472f8182579663ab39d4
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-29 09:27:59 -0600

    Add munchies request spec for seeing a list of open restaurants

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -1,10 +1,33 @@
-# frozen_string_literal:true
+# frozen_string_literal: true
 
 require 'rails_helper'
 
 RSpec.describe 'Munchies API endpoint' do
   it 'delivers a list of restaurants that will be open at arrival time when a valid api key is sent' do
+    user_params = { email: 'user@example.com',
+                    password: 'password' }
 
+    user = User.create!(user_params)
+
+    params = {
+      start: 'denver,co',
+      end: 'pueblo,co',
+      food: 'chinese',
+      api_key: user.api_key
+    }
+
+    get api_v1_munchies_path, params: params
+
+    munchies = JSON.parse(response.body, symbolize_names: true)
+
+    expect(munchies[:start]).to eq('Denver, CO, USA')
+    expect(munchies[:end]).to eq('Pueblo, CO, USA')
+    expect(munchies[:food]).to eq('chinese')
+
+    expect(munchies).to have_key(:open_restaurants)
+    expect(munchies[:open_restaurants].count).to eq(3)
+    expect(munchies[:open_restaurants].first).to have_key(:name)
+    expect(munchies[:open_restaurants].first).to have_key(:address)
   end
 
   it 'returns 401 when an invalid api key is sent' do
@@ -15,7 +38,7 @@ RSpec.describe 'Munchies API endpoint' do
       api_key: 'jgn983hy48thw9begh98h4539h4'
     }
 
-    post api_v1_munchies_path, params: params
+    get api_v1_munchies_path, params: params
 
     expect(response.status).to eq(401)
 
@@ -25,19 +48,14 @@ RSpec.describe 'Munchies API endpoint' do
   end
 end
 
-# GET /api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese Content-Type: application/json Accept: application/json
-#
-# You will use the Google Directions API: https://developers.google.com/maps/documentation/directions/start
-# in order to find out how long it will take to travel from the two locations, and then using
-# the Yelp API, you will find all of the restaurants matching the cuisine, the example here is Chinese,
-# that WILL BE OPEN at your estimated time of arrival.
-#
-# Your API will return the end city, and three restaurants, along with their name and address.
-#
+# You will use the Google Directions API: https://developers.google.com/maps/documentation/directions/start in order to
+# find out how long it will take to travel from the two locations, and then using the Yelp API, you will find all of
+# the restaurants matching the cuisine, the example here is Chinese, that WILL BE OPEN at your estimated time of arrival.
+
 # Technical notes:
 #
-# The Yelp API only accepts time as UNIX time. You can conert a Time object into UNIX time
-# by doing something like this: Time.now.to_i
+# The Yelp API only accepts time as UNIX time. You can conert a Time
+# object into UNIX time by doing something like this: Time.now.to_i
 #
-# You can find out time in the future using a feature built into Rails' ActiveSupport which
-# will let you do things like this: Time.now + 4.hours (This will ONLY work in Rails and not a pry session run from the command line)
+# You can find out time in the future using a feature built into Rails' ActiveSupport which will let you do things like
+# this: Time.now + 4.hours (This will ONLY work in Rails and not a pry session run from the command line)
