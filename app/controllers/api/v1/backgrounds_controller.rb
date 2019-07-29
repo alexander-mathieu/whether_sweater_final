08d9f7a8e5eced7c940e9cb112016c3c991f7b19
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-28 20:09:20 -0600

    Update BackgroundController to reflect new ForecastFacade functionality

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -2,7 +2,13 @@
 
 class Api::V1::BackgroundsController < ApplicationController
   def show
-    facade = ForecastFacade.new(params[:location])
+    facade = ForecastFacade.new(location_params)
     render json: BackgroundSerializer.serialize(facade.background_images)
+  end
+
+  private
+
+  def location_params
+    params.permit(:location)
   end
 end
