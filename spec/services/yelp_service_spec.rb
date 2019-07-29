23135c22642bde4535e69f4d4541abd99994bf76
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-29 11:38:23 -0600

    Cleanup YelpService spec

---
+++ spec/services/yelp_service_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe YelpService do
+  describe 'instance methods' do
+    it '#retrieve_open_restaurants' do
+      open_restaurants = subject.retrieve_open_restaurants('pueblo,co', 'chinese', 1564430400)
+
+      expect(open_restaurants).to be_an(Array)
+      expect(open_restaurants.first).to have_key(:name)
+      expect(open_restaurants.first[:location]).to have_key(:display_address)
+    end
+  end
+end
