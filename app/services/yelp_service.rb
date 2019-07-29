a25c5a09928a4a1a47d4d355265c28dc29b07560
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-29 10:13:24 -0600

    Add YelpService retrieve_restaurants and supporting methods

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,2 +1,24 @@
 class YelpService
+  def retrieve_restaurants(location, food)
+    params = {
+      location: location,
+      categories: food,
+    }
+
+    parse_response('search', params)
+  end
+
+  private
+
+  def parse_response(url, params = {})
+    response = connection.get(url, params)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def connection
+    Faraday.new('https://api.yelp.com/v3/businesses') do |f|
+      f.headers['Authorization'] = "Bearer #{ENV['YELP_API_KEY']}"
+      f.adapter Faraday.default_adapter
+    end
+  end
 end
