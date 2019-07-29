79bbe1697f00e9f2c00d6fdd86c6e96ae4c1784c
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-29 11:00:41 -0600

    Refactor RestaurantForecastSerializer to use appropriate key

--- app/serializers/restaurant_forecast_serializer.rb
+++ app/serializers/restaurant_forecast_serializer.rb
@@ -2,21 +2,20 @@ class RestaurantForecastSerializer
   def self.serialize(restaurant_forecast)
     {
       data: {
-        start: restaurant_forecast.start,
-        end: restaurant_forecast.end,
+        destination: restaurant_forecast.destination,
         food: restaurant_forecast.food,
         open_restaurants: [
           {
-            name: restaurant_forecast.open_restaurants[0].name,
-            address: restaurant_forecast.open_restaurants[0].address
+            name: restaurant_forecast.open_restaurants[0][:name],
+            address: restaurant_forecast.open_restaurants[0][:location][:display_address]
           },
           {
-            name: restaurant_forecast.open_restaurants[1].name,
-            address: restaurant_forecast.open_restaurants[1].address
+            name: restaurant_forecast.open_restaurants[1][:name],
+            address: restaurant_forecast.open_restaurants[1][:location][:display_address]
           },
           {
-            name: restaurant_forecast.open_restaurants[2].name,
-            address: restaurant_forecast.open_restaurants[2].address
+            name: restaurant_forecast.open_restaurants[2][:name],
+            address: restaurant_forecast.open_restaurants[2][:location][:display_address]
           }
         ]
       }
