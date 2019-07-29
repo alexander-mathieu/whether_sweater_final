491e67213ed30cb6c980ee9a0f4bc80ca1189a14
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-29 10:27:31 -0600

    Add basic structure for RestaurantForecastSerializer serialize method

--- app/serializers/restaurant_forecast_serializer.rb
+++ app/serializers/restaurant_forecast_serializer.rb
@@ -1,5 +1,25 @@
 class RestaurantForecastSerializer
-  def serialize(restaurant_forecast)
-    
+  def self.serialize(restaurant_forecast)
+    {
+      data: {
+        start: restaurant_forecast.start,
+        end: restaurant_forecast.end,
+        food: restaurant_forecast.food,
+        open_restaurants: [
+          {
+            name: restaurant_forecast.open_restaurants[0].name,
+            address: restaurant_forecast.open_restaurants[0].address
+          },
+          {
+            name: restaurant_forecast.open_restaurants[1].name,
+            address: restaurant_forecast.open_restaurants[1].address
+          },
+          {
+            name: restaurant_forecast.open_restaurants[2].name,
+            address: restaurant_forecast.open_restaurants[2].address
+          }
+        ]
+      }
+    }
   end
 end
