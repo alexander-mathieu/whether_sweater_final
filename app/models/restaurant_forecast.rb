2c33f66013f780d2fa735445093faa786ce1d01c
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-29 10:27:03 -0600

    Add RestaurantForecast initialize, arrival_time and open_restaurants method

--- app/models/restaurant_forecast.rb
+++ app/models/restaurant_forecast.rb
@@ -1,6 +1,26 @@
 class RestaurantForecast
-  def initialize(road_trip_info, restaurants)
+  attr_reader :start, :end, :food
+
+  def initialize(road_trip_info, food, restaurants)
     @road_trip_info = road_trip_info
     @restaurants = restaurants
+    @start = road_trip_info[:origin]
+    @end = road_trip_info[:destination]
+    @food = food
+    @travel_time = road_trip_info[:value]
+  end
+
+  def open_restaurants
+    restaurants.map do |restaurant|
+      require 'pry'; binding.pry
+    end
+  end
+
+  private
+
+  attr_reader :travel_time, :restaurants
+
+  def arrival_time
+    travel_time + Time.now.to_i
   end
 end
