03d577f66d404d7ff8cc19c608b81c2463b5565c
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-29 19:37:38 -0600

    Refactor Background/ForecastSerializers

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -16,82 +16,24 @@ class ForecastSerializer
           visibility_miles: location_forecast.currently[:visibility].round(2),
           uv_index: location_forecast.currently[:uvIndex]
         },
-        hourly: [
-          {
-            time: Time.at(location_forecast.hourly[0][:time]).to_datetime.strftime('%I:%M%p'),
-            temperature: location_forecast.hourly[0][:temperature].round
-          },
-          {
-            time: Time.at(location_forecast.hourly[1][:time]).to_datetime.strftime('%I:%M%p'),
-            temperature: location_forecast.hourly[1][:temperature].round
-          },
-          {
-            time: Time.at(location_forecast.hourly[2][:time]).to_datetime.strftime('%I:%M%p'),
-            temperature: location_forecast.hourly[2][:temperature].round
-          },
-          {
-            time: Time.at(location_forecast.hourly[3][:time]).to_datetime.strftime('%I:%M%p'),
-            temperature: location_forecast.hourly[3][:temperature].round
-          },
-          {
-            time: Time.at(location_forecast.hourly[4][:time]).to_datetime.strftime('%I:%M%p'),
-            temperature: location_forecast.hourly[4][:temperature].round
-          },
-          {
-            time: Time.at(location_forecast.hourly[5][:time]).to_datetime.strftime('%I:%M%p'),
-            temperature: location_forecast.hourly[5][:temperature].round
-          },
-          {
-            time: Time.at(location_forecast.hourly[6][:time]).to_datetime.strftime('%I:%M%p'),
-            temperature: location_forecast.hourly[6][:temperature].round
-          },
-          {
-            time: Time.at(location_forecast.hourly[7][:time]).to_datetime.strftime('%I:%M%p'),
-            temperature: location_forecast.hourly[7][:temperature].round
-          }
-        ],
-        daily: [
-          {
-            day: Time.at(location_forecast.daily[1][:time]).to_datetime.strftime('%A'),
-            summary: location_forecast.daily[1][:summary],
-            icon: location_forecast.daily[1][:icon],
-            percent_humidity: (location_forecast.daily[1][:humidity] * 100).to_i,
-            forecast_high: location_forecast.daily[1][:temperatureHigh].round,
-            forecast_low: location_forecast.daily[1][:temperatureLow].round
-          },
-          {
-            day: Time.at(location_forecast.daily[2][:time]).to_datetime.strftime('%A'),
-            summary: location_forecast.daily[2][:summary],
-            icon: location_forecast.daily[2][:icon],
-            percent_humidity: (location_forecast.daily[2][:humidity] * 100).to_i,
-            forecast_high: location_forecast.daily[2][:temperatureHigh].round,
-            forecast_low: location_forecast.daily[2][:temperatureLow].round
-          },
-          {
-            day: Time.at(location_forecast.daily[3][:time]).to_datetime.strftime('%A'),
-            summary: location_forecast.daily[3][:summary],
-            icon: location_forecast.daily[3][:icon],
-            percent_humidity: (location_forecast.daily[3][:humidity] * 100).to_i,
-            forecast_high: location_forecast.daily[3][:temperatureHigh].round,
-            forecast_low: location_forecast.daily[3][:temperatureLow].round
-          },
-          {
-            day: Time.at(location_forecast.daily[4][:time]).to_datetime.strftime('%A'),
-            summary: location_forecast.daily[4][:summary],
-            icon: location_forecast.daily[4][:icon],
-            percent_humidity: (location_forecast.daily[4][:humidity] * 100).to_i,
-            forecast_high: location_forecast.daily[4][:temperatureHigh].round,
-            forecast_low: location_forecast.daily[4][:temperatureLow].round
-          },
-          {
-            day: Time.at(location_forecast.daily[5][:time]).to_datetime.strftime('%A'),
-            summary: location_forecast.daily[5][:summary],
-            icon: location_forecast.daily[5][:icon],
-            percent_humidity: (location_forecast.daily[5][:humidity] * 100).to_i,
-            forecast_high: location_forecast.daily[5][:temperatureHigh].round,
-            forecast_low: location_forecast.daily[5][:temperatureLow].round
-          }
-        ]
+        hourly:
+          location_forecast.hourly.map do |forecast|
+            {
+              time: Time.at(forecast[:time]).to_datetime.strftime('%I:%M%p'),
+              temperature: forecast[:temperature].round
+            }
+          end,
+        daily:
+          location_forecast.daily.map do |forecast|
+            {
+              day: Time.at(forecast[:time]).to_datetime.strftime('%A'),
+              summary: forecast[:summary],
+              icon: forecast[:icon],
+              percent_humidity: (forecast[:humidity] * 100).to_i,
+              forecast_high: forecast[:temperatureHigh].round,
+              forecast_low: forecast[:temperatureLow].round
+            }
+          end
       }
     }
   end
