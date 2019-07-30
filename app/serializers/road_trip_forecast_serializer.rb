a2f4cbc942cc787b0e97a28f28465ecfe1752cfc
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-30 13:50:41 -0600

    Remove commented code from RoadTripForecastSerializer

--- app/serializers/road_trip_forecast_serializer.rb
+++ app/serializers/road_trip_forecast_serializer.rb
@@ -14,21 +14,4 @@ class RoadTripForecastSerializer
              :temperature,
              :uv_index,
              :visibility_miles
-  # def self.serialize(road_trip_forecast)
-  #   {
-  #     data: {
-  #       origin: road_trip_forecast.origin,
-  #       destination: road_trip_forecast.destination,
-  #       weather_on_arrival: {
-  #         summary: road_trip_forecast.summary,
-  #         icon: road_trip_forecast.icon,
-  #         temperature: road_trip_forecast.temperature.round,
-  #         feels_like: road_trip_forecast.feels_like.round,
-  #         percent_humidity: road_trip_forecast.percent_humidity,
-  #         visibility_miles: road_trip_forecast.visibility_miles.round(2),
-  #         uv_index: road_trip_forecast.uv_index
-  #       }
-  #     }
-  #   }
-  # end
 end
