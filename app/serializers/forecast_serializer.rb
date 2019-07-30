f4034de5061c7d4af019872c3c90559e0793eb1b
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-29 20:18:30 -0600

    Refactor ForecastSerializer and LocationForecast model

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -4,36 +4,20 @@ class ForecastSerializer
       data: {
         location: location_forecast.location_address,
         currently: {
-          date: Time.at(location_forecast.currently[:time]).to_datetime.strftime('%m-%d'),
-          time: Time.at(location_forecast.currently[:time]).to_datetime.strftime('%I:%M%p'),
-          summary: location_forecast.currently[:summary],
-          icon: location_forecast.currently[:icon],
-          temperature: location_forecast.currently[:temperature].round,
-          forecast_high: location_forecast.daily[0][:temperatureHigh].round,
-          forecast_low: location_forecast.daily[0][:temperatureLow].round,
-          feels_like: location_forecast.currently[:apparentTemperature].round,
-          percent_humidity: (location_forecast.currently[:humidity] * 100).to_i,
-          visibility_miles: location_forecast.currently[:visibility].round(2),
-          uv_index: location_forecast.currently[:uvIndex]
+          date: location_forecast.current_date,
+          time: location_forecast.current_time,
+          summary: location_forecast.current_summary,
+          icon: location_forecast.current_icon,
+          temperature: location_forecast.current_temp.round,
+          forecast_high: location_forecast.daily_high.round,
+          forecast_low: location_forecast.daily_low.round,
+          feels_like: location_forecast.current_apparent_temp.round,
+          percent_humidity: location_forecast.current_percent_humidity,
+          visibility_miles: location_forecast.current_visibility.round(2),
+          uv_index: location_forecast.current_uv_index
         },
-        hourly:
-          location_forecast.hourly.map do |forecast|
-            {
-              time: Time.at(forecast[:time]).to_datetime.strftime('%I:%M%p'),
-              temperature: forecast[:temperature].round
-            }
-          end,
-        daily:
-          location_forecast.daily.map do |forecast|
-            {
-              day: Time.at(forecast[:time]).to_datetime.strftime('%A'),
-              summary: forecast[:summary],
-              icon: forecast[:icon],
-              percent_humidity: (forecast[:humidity] * 100).to_i,
-              forecast_high: forecast[:temperatureHigh].round,
-              forecast_low: forecast[:temperatureLow].round
-            }
-          end
+        hourly: location_forecast.hourly_forecast,
+        daily: location_forecast.daily_forecast
       }
     }
   end
