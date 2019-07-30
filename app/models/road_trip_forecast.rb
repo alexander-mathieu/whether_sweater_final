1d869adf3dad6dad7f61e2ade2f84068869dd71b
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-29 19:52:05 -0600

    Refactor RoadTripForecast model and serializer

--- app/models/road_trip_forecast.rb
+++ app/models/road_trip_forecast.rb
@@ -10,15 +10,43 @@ class RoadTripForecast
     @forecast = forecast
   end
 
-  def weather_on_arrival
-    @weather_on_arrival ||= forecast[:hourly][:data].min do |forecast|
-      (time_on_arrival - forecast[:time]).abs
-    end
+  def icon
+    weather_on_arrival[:icon]
+  end
+
+  def feels_like
+    weather_on_arrival[:apparentTemperature]
+  end
+
+  def percent_humidity
+    (weather_on_arrival[:humidity] * 100).to_i
+  end
+
+  def summary
+    weather_on_arrival[:summary]
+  end
+
+  def temperature
+    weather_on_arrival[:temperature]
+  end
+
+  def uv_index
+    weather_on_arrival[:uvIndex]
+  end
+
+  def visibility_miles
+    weather_on_arrival[:visibility]
   end
 
   private
 
   attr_reader :duration_in_seconds, :forecast
+
+  def weather_on_arrival
+    @weather_on_arrival ||= forecast[:hourly][:data].min do |forecast|
+      (time_on_arrival - forecast[:time]).abs
+    end
+  end
 
   def time_on_arrival
     duration_in_seconds + Time.now.to_i
