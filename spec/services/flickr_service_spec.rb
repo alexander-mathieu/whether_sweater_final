46ce52195e526241937273113998bed3b65a299f
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-29 21:23:46 -0600

    Update GoogleMaps/FlickrService specs to reflect method name changes

--- spec/services/flickr_service_spec.rb
+++ spec/services/flickr_service_spec.rb
@@ -4,10 +4,10 @@ require 'rails_helper'
 
 RSpec.describe FlickrService do
   describe 'instance methods' do
-    it '#images_search' do
+    it '#retrieve_images' do
       flickr_service = FlickrService.new('39.7392358', '-104.990251')
 
-      response = flickr_service.images_search
+      response = flickr_service.retrieve_images
 
       expect(response).to be_an(Array)
       expect(response[0]).to have_key(:id)
