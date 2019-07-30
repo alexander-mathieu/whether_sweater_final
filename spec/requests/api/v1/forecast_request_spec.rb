29dc3edb14cb5f645a5a529163a0a856053de575
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-30 11:44:35 -0600

    Add VCR to forecast api endpoint spec

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -3,19 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Forecast API endpoint' do
-  it 'delivers the forecast for a specific location' do\
-    get api_v1_forecast_path, params: { location: 'denver,co' }
+  it 'delivers the forecast for a specific location' do
 
-    expect(response).to be_successful
+    VCR.use_cassette('forecast_api_endpoint') do
+      get api_v1_forecast_path, params: { location: 'denver,co' }
 
-    forecast = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(response).to be_successful
 
-    expect(forecast).to have_key(:location)
-    expect(forecast).to have_key(:currently)
-    expect(forecast).to have_key(:hourly)
-    expect(forecast).to have_key(:daily)
+      forecast = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    expect(forecast[:hourly].count).to eq(8)
-    expect(forecast[:daily].count).to eq(5)
+      expect(forecast).to have_key(:location)
+      expect(forecast).to have_key(:currently)
+      expect(forecast).to have_key(:hourly)
+      expect(forecast).to have_key(:daily)
+
+      expect(forecast[:hourly].count).to eq(8)
+      expect(forecast[:daily].count).to eq(5)
+    end
   end
 end
