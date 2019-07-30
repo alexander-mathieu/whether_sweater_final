d00770a3c2d90e371647f5d2f3ae11bd7cbebec1
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-30 11:34:49 -0600

    Add VCR to DarkskyService spec

--- spec/services/darksky_service_spec.rb
+++ spec/services/darksky_service_spec.rb
@@ -7,17 +7,19 @@ RSpec.describe DarkskyService do
     it '#retrieve_forecast' do
       darksky_service = DarkskyService.new('39.7392358,-104.990251')
 
-      response = darksky_service.retrieve_forecast
+      VCR.use_cassette('darksky_service_response') do
+        response = darksky_service.retrieve_forecast
 
-      expect(response).to be_a(Hash)
+        expect(response).to be_a(Hash)
 
-      expect(response[:currently]).to have_key(:temperature)
-      expect(response[:currently]).to have_key(:apparentTemperature)
-      expect(response[:currently]).to have_key(:humidity)
-      expect(response[:currently]).to have_key(:uvIndex)
+        expect(response[:currently]).to have_key(:temperature)
+        expect(response[:currently]).to have_key(:apparentTemperature)
+        expect(response[:currently]).to have_key(:humidity)
+        expect(response[:currently]).to have_key(:uvIndex)
 
-      expect(response[:hourly][:data]).to be_an(Array)
-      expect(response[:daily][:data]).to be_an(Array)
+        expect(response[:hourly][:data]).to be_an(Array)
+        expect(response[:daily][:data]).to be_an(Array)
+      end
     end
   end
 end
