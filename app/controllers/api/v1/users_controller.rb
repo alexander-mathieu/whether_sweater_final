ae82c3dada5ad36fb6804c22a2e065f91a85b54a
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-30 18:45:46 -0600

    Change ApplicationController to inherit from ActionController::API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Api::V1::UsersController < ApplicationController
-  protect_from_forgery with: :null_session
-
   def create
     user = User.new(user_params)
 
