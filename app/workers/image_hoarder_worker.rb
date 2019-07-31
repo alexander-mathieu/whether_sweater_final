598c59ea4e86de55f90ac1325d83548a7778cec0
Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
2019-07-31 11:36:49 -0600

    Add ImageHoarderWorker and hoard_background_images rake task

---
+++ app/workers/image_hoarder_worker.rb
@@ -0,0 +1,21 @@
+class ImageHoarderWorker
+  include Sidekiq::Worker
+
+  def perform
+    top_20_cities.each do |city|
+      city_params = { location: city }
+      background_facade = BackgroundFacade.new(city_params)
+      background_facade.location_images
+    end
+  end
+
+  private
+
+  def top_20_cities
+    ['new york, ny', 'los angeles, ca', 'chicago, il', 'houston, tx',
+    'philadelphia, pa', 'pheonix, az', 'san antonio, tx', 'san diego, ca',
+    'dallas, tx', 'san jose, ca', 'austin, tx', 'jacksonville, fl',
+    'san francisco, ca', 'indianapololis, in', 'columbus, oh', 'fort worth, tx',
+    'charlotte, nc', 'detroit, mi', 'el paso, tx', 'seattle, wa']
+  end
+end
---
+++ lib/tasks/scheduler.rake
@@ -0,0 +1,5 @@
+task :hoard_background_images => :environment do
+  puts 'Hoarding background images...'
+  ImageHoarderWorker.new.perform
+  puts 'Background images stashed!'
+end
